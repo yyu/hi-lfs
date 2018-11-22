3f3bdd9aedabf9bd144833538a0e334d73c2c869
y² <yy@yuyuan.org>
2018-11-21 23:12:51 -0800

    before entering the chroot environment

--- 8.3-systemd/000-functions.sh
+++ 8.3-systemd/000-functions.sh
@@ -1214,6 +1214,106 @@ _lfs_after_chapter5() {
 }
 
 ################################################################################
+# 6.2. Preparing Virtual Kernel File Systems
+
+# Various file systems exported by the kernel are used to communicate to and from the kernel itself.
+# These file systems are virtual in that no disk space is used for them.
+# The content of the file systems resides in memory.
+
+_lfs_create_directories_dev_proc_sys_run() {
+    # Begin by creating directories onto which the file systems will be mounted:
+    mkdir -pv $LFS/{dev,proc,sys,run}
+}
+
+_lfs_create_initial_device_nodes() {
+    # 6.2.1. Creating Initial Device Nodes
+    # When the kernel boots the system, it requires the presence of a few device nodes, in particular the console and null devices.
+    # The device nodes must be created on the hard disk so that they are available before udevd has been started,
+    # and additionally when Linux is started with init=/bin/bash.
+    mknod -m 600 $LFS/dev/console c 5 1
+    mknod -m 666 $LFS/dev/null c 1 3
+}
+
+_lfs_mount_and_populate_dev() {
+    # 6.2.2. Mounting and Populating /dev
+
+    # The recommended method of populating the /dev directory with devices is
+    # to mount a virtual filesystem (such as tmpfs) on the /dev directory,
+    # and allow the devices to be created dynamically on that virtual filesystem
+    # as they are detected or accessed.
+
+    # Device creation is generally done during the boot process by Udev.
+    # Since this new system does not yet have Udev and has not yet been booted,
+    # it is necessary to mount and populate /dev manually.
+    # This is accomplished by bind mounting the host system's /dev directory.
+    # A bind mount is a special type of mount that allows you to create a mirror
+    # of a directory or mount point to some other location.
+    mount -v --bind /dev $LFS/dev
+}
+
+_lfs_mount_virtual_kernel_fs() {
+    # 6.2.3. Mounting Virtual Kernel File Systems
+    mount -vt devpts devpts $LFS/dev/pts -o gid=5,mode=620
+    mount -vt proc proc $LFS/proc
+    mount -vt sysfs sysfs $LFS/sys
+    mount -vt tmpfs tmpfs $LFS/run
+
+    # In some host systems, /dev/shm is a symbolic link to /run/shm.
+    # The /run tmpfs was mounted above so in this case only a directory needs to be created.
+    if [ -h $LFS/dev/shm ]; then
+      mkdir -pv $LFS/$(readlink $LFS/dev/shm)
+    fi
+}
+
+################################################################################
+# 6.4. Entering the Chroot Environment
+
+# It is time to enter the chroot environment to begin building and installing the final LFS system.
+
+_lfs_enter_chroot_env() {
+    # As user root, run the following command to enter the realm that is,
+    # at the moment, populated with only the temporary tools:
+
+    chroot "$LFS" /tools/bin/env -i \
+        HOME=/root                  \
+        TERM="$TERM"                \
+        PS1='(lfs chroot) \u:\w\$ ' \
+        PATH=/bin:/usr/bin:/sbin:/usr/sbin:/tools/bin \
+        /tools/bin/bash --login +h
+
+    # The -i option given to the env command will clear all variables of the chroot environment.
+    # After that, only the HOME, TERM, PS1, and PATH variables are set again.
+    # The TERM=$TERM construct will set the TERM variable inside chroot to the same value as outside chroot.
+    # This variable is needed for programs like vim and less to operate properly.
+
+    # If other variables are needed, such as CFLAGS or CXXFLAGS, this is a good place to set them again.
+
+    # From this point on, there is no need to use the LFS variable anymore,
+    # because all work will be restricted to the LFS file system.
+    # This is because the Bash shell is told that $LFS is now the root (/) directory.
+
+    # Notice that /tools/bin comes last in the PATH.
+    # This means that a temporary tool will no longer be used once its final version is installed.
+    # This occurs when the shell does not “remember” the locations of executed binaries—for this reason,
+    # hashing is switched off by passing the +h option to bash.
+
+    # Note that the bash prompt will say I have no name!
+    # This is normal because the /etc/passwd file has not been created yet.
+}
+
+_lfs_note_about_chroot() {
+    echo -e "\
+        | \033[1;7;33mNote\033[0;1m\033[33m__________________________________________________\033[0m
+        | \033[1;33mIt is important that all the commands throughout the remainder of this chapter
+        | and the following chapters are run from within the chroot environment\033[0m.
+        | \033[1;33mIf you leave this environment for any reason (rebooting for example),
+        | ensure that the virtual kernel filesystems are mounted as explained in Section 6.2.2,
+        | “Mounting and Populating /dev” and Section 6.2.3, “Mounting Virtual Kernel File Systems”
+        | and enter chroot again before continuing with the installation\033[0m.
+        | " | sed -E 's/^ *\| //g'
+}
+
+################################################################################
 # example
 _lfs_install_() {
     package=""
