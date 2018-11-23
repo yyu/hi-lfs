5f483f3aba8906fb65d839d5f5331cc2c9cdd250
y² <yy@yuyuan.org>
2018-11-22 16:24:42 -0800

    6.21. GCC-8.2.0

--- 8.3-systemd/000-functions.sh
+++ 8.3-systemd/000-functions.sh
@@ -2399,6 +2399,197 @@ _lfs_post_chroot_configure_shadow() {
 }
 
 ################################################################################
+# 6.21. GCC-8.2.0
+
+_lfs_post_chroot_install_gcc() {
+    set +e
+
+    package________name="gcc"
+    cd /sources/
+    tar xf `ls $package________name-*tar*`
+    cd $package________name-*[0-9]
+    ________________________________________________________________________________ '
+    If building on x86_64, change the default directory name for 64-bit libraries to “lib”:
+    '
+    case $(uname -m) in
+      x86_64)
+        sed -e '/m64=/s/lib64/lib/' \
+            -i.orig gcc/config/i386/t-linux64
+      ;;
+    esac
+    ________________________________________________________________________________ '
+    Remove the symlink created earlier as the final gcc includes will be installed here:
+    '
+    rm -f /usr/lib/gcc
+    ________________________________________________________________________________ '
+    The GCC documentation recommends building GCC in a dedicated build directory:
+    '
+    mv -v build build____
+    mkdir -v build
+    cd       build
+    ________________________________________________________________________________ '
+    Prepare GCC for compilation:
+    '
+    SED=sed                               \
+    ../configure --prefix=/usr            \
+                 --enable-languages=c,c++ \
+                 --disable-multilib       \
+                 --disable-bootstrap      \
+                 --disable-libmpx         \
+                 --with-system-zlib
+    ________________________________________________________________________________ '
+    compile
+    '
+    make
+    ________________________________________IMPORTANT________________________________________ '
+    In this section, the test suite for GCC is considered critical. Do not skip it under any circumstance.
+    '
+    ________________________________________________________________________________ '
+    One set of tests in the GCC test suite is known to exhaust the stack, so increase the stack size prior to running the tests:
+    '
+    ulimit -s 32768
+    ________________________________________________________________________________ '
+    Remove one test known to cause a problem:
+    '
+    rm ../gcc/testsuite/g++.dg/pr83239.C
+    ________________________________________________________________________________ '
+    Test the results as a non-privileged user, but do not stop at errors:
+    '
+    chown -Rv nobody . 
+    su nobody -s /bin/bash -c "PATH=$PATH make -k check"
+    ________________________________________________________________________________ '
+    To receive a summary of the test suite results, run:
+    '
+    ../contrib/test_summary
+    ________________________________________________________________________________ '
+    For only the summaries, pipe the output through grep -A7 Summ.
+    '
+    ../contrib/test_summary | grep -A7 Summ
+    ________________________________________________________________________________ '
+    Results can be compared with those located at http://www.linuxfromscratch.org/lfs/build-logs/8.3/ and https://gcc.gnu.org/ml/gcc-testresults/.
+    A few unexpected failures cannot always be avoided.
+    The GCC developers are usually aware of these issues, but have not resolved them yet.
+    Unless the test results are vastly different from those at the above URL, it is safe to continue.
+    '
+    ________________________________________NOTE________________________________________ '
+    On some combinations of kernel configuration and AMD processors there may be more than 1100 failures
+    in the gcc.target/i386/mpx tests (which are designed to test the MPX option on recent Intel processors).
+    These can safely be ignored on AMD processors.
+    These tests will also fail on Intel processors if MPX support is not enabled in the kernel even though it is present on the CPU.
+    '
+    ________________________________________________________________________________ '
+    make install
+    '
+    make install
+    ________________________________________________________________________________ '
+    Create a symlink required by the FHS for "historical" reasons.
+    '
+    ln -sv ../usr/bin/cpp /lib
+    ________________________________________________________________________________ '
+    Many packages use the name cc to call the C compiler. To satisfy those packages, create a symlink:
+    '
+    ln -sv gcc /usr/bin/cc
+    ________________________________________________________________________________ '
+    Add a compatibility symlink to enable building programs with Link Time Optimization (LTO):
+    '
+    install -v -dm755 /usr/lib/bfd-plugins
+    ln -sfv ../../libexec/gcc/$(gcc -dumpmachine)/8.2.0/liblto_plugin.so \
+            /usr/lib/bfd-plugins/
+    ________________________________________________________________________________ '
+    Now that our final toolchain is in place, it is important to again ensure that compiling and linking will work as expected.
+    We do this by performing the same sanity checks as we did earlier in the chapter:
+    '
+    echo 'int main(){}' > dummy.c
+    cc dummy.c -v -Wl,--verbose &> dummy.log
+    readelf -l a.out | grep ': /lib'
+    ________________________________________there_should_have________________________________________ '
+    There should be no errors, and the output of the last command will be (allowing for platform-specific differences in dynamic linker name):' '
+    [Requesting program interpreter: /lib64/ld-linux-x86-64.so.2]
+    '
+    ________________________________________________________________________________ '
+    Now make sure that we'"'"'re setup to use the correct start files:
+    '
+    grep -o '/usr/lib.*/crt[1in].*succeeded' dummy.log
+    ________________________________________there_should_have________________________________________ '
+    The output of the last command should be:' '
+    /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.0/../../../../lib/crt1.o succeeded
+    /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.0/../../../../lib/crti.o succeeded
+    /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.0/../../../../lib/crtn.o succeeded' '
+    Depending on your machine architecture, the above may differ slightly,
+    the difference usually being the name of the directory after /usr/lib/gcc.' '
+    The important thing to look for here is that gcc has found all three crt*.o files under the /usr/lib directory.
+    '
+    ________________________________________________________________________________ '
+    Verify that the compiler is searching for the correct header files:
+    '
+    grep -B4 '^ /usr/include' dummy.log
+    ________________________________________there_should_have________________________________________ '
+    This command should return the following output:' '
+    #include <...> search starts here:
+     /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.0/include
+     /usr/local/include
+     /usr/lib/gcc/x86_64-pc-linux-gnu/8.2.0/include-fixed
+     /usr/include' '
+    Again, note that the directory named after your target triplet may be different than the above, depending on your architecture.
+    '
+    ________________________________________________________________________________ '
+    Next, verify that the new linker is being used with the correct search paths:
+    '
+    grep 'SEARCH.*/usr/lib' dummy.log |sed 's|; |\n|g'
+    ________________________________________there_should_have________________________________________ '
+    References to paths that have components with '-linux-gnu' should be ignored, but otherwise the output of the last command should be:' '
+    SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib64")
+    SEARCH_DIR("/usr/local/lib64")
+    SEARCH_DIR("/lib64")
+    SEARCH_DIR("/usr/lib64")
+    SEARCH_DIR("/usr/x86_64-pc-linux-gnu/lib")
+    SEARCH_DIR("/usr/local/lib")
+    SEARCH_DIR("/lib")
+    SEARCH_DIR("/usr/lib");' '
+    A 32-bit system may see a few different directories. For example, here is the output from an i686 machine:' '
+    SEARCH_DIR("/usr/i686-pc-linux-gnu/lib32")
+    SEARCH_DIR("/usr/local/lib32")
+    SEARCH_DIR("/lib32")
+    SEARCH_DIR("/usr/lib32")
+    SEARCH_DIR("/usr/i686-pc-linux-gnu/lib")
+    SEARCH_DIR("/usr/local/lib")
+    SEARCH_DIR("/lib")
+    SEARCH_DIR("/usr/lib");
+    '
+    ________________________________________________________________________________ '
+    Next make sure that we'"'"'re using the correct libc:
+    '
+    grep "/lib.*/libc.so.6 " dummy.log
+    ________________________________________there_should_have________________________________________ '
+    The output of the last command should be:' '
+    attempt to open /lib/libc.so.6 succeeded
+    '
+    ________________________________________________________________________________ '
+    Lastly, make sure GCC is using the correct dynamic linker:
+    '
+    grep found dummy.log
+    ________________________________________there_should_have________________________________________ '
+    The output of the last command should be (allowing for platform-specific differences in dynamic linker name): ' '
+    found ld-linux-x86-64.so.2 at /lib/ld-linux-x86-64.so.2 ' '
+    If the output does not appear as shown above or is not received at all, then something is seriously wrong.
+    Investigate and retrace the steps to find out where the problem is and correct it.
+    The most likely reason is that something went wrong with the specs file adjustment.
+    Any issues will need to be resolved before continuing with the process.
+    '
+    ________________________________________________________________________________ '
+    Once everything is working correctly, clean up the test files:
+    '
+    rm -v dummy.c a.out dummy.log
+    ________________________________________________________________________________ '
+    Finally, move a misplaced file:
+    '
+    mkdir -pv /usr/share/gdb/auto-load/usr/lib
+    mv -v /usr/lib/*gdb.py /usr/share/gdb/auto-load/usr/lib
+
+    set +e
+}
+
+################################################################################
 # example
 _lfs_post_chroot_install_() {
     package________name=""
