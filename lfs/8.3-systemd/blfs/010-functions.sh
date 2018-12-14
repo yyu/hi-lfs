94e636f61d728f0350b7f19ab1899856bd865666
y² <yy@yuyuan.org>
2018-12-13 22:23:48 -0800

    _blfs_add_user_yyu

--- lfs/8.3-systemd/blfs/010-functions.sh
+++ lfs/8.3-systemd/blfs/010-functions.sh
@@ -152,7 +152,7 @@ _blfs_install_wget() {
     if [ x"$ans" == xy ]; then
         ./configure --prefix=/usr --sysconfdir=/etc --with-ssl=openssl
         make
-        make install        
+        make install
     fi
 }
 
@@ -199,7 +199,7 @@ Persistent=true
 WantedBy=timers.target
 EOF
 
-    systemctl enable update-pciids.timer    
+    systemctl enable update-pciids.timer
 }
 
 _blfs_test() {
@@ -214,131 +214,82 @@ _blfs_install_vimcat() {
 
 _blfs_set_bashrc() {
     cat > ~/.bashrc << "EOF"
-function parse_git_branch {
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/\* \(.*\)/(\1)/'
-}
-# colors in terminals
-
-export NOCOLOR='\033[00m'
-
-export BLACK='\033[0;1m'
-
-export RED='\033[0;31m'
-export REDUL='\033[0;4;31m'
-
-export GREEN='\033[0;32m'
-export GREENUL='\033[0;4;32m'
-export LIGHTGREEN='\033[0;1;32m'
-
-export YELLOW='\033[0;33m'
-export YELLOWUL='\033[0;4;33m'
-export LIGHTYELLOW='\033[0;1;33m'
-
-export BLUE='\033[0;34m'
-export BLUEUL='\033[0;4;34m'
-export LIGHTBLUE='\033[0;1;34m'
-
-export MAGENTA='\033[0;35m'
-export MAGENTAUL='\033[0;4;35m'
-export LIGHTMAGENTA='\033[0;1;35m'
-
-export CYAN='\033[0;36m'
-export CYANUL='\033[0;4;36m'
-export LIGHTCYAN='\033[0;1;36m'
-
-export GRAY='\033[0;37m'
-export GRAYUL='\033[0;4;37m'
-
-export WHITE='\033[0;1;37m'
-
-# colors on gray
-
-export BLACK_ON_GRAY='\033[0;47;1m'
-
-export RED_ON_GRAY='\033[0;47;31m'
-export REDUL_ON_GRAY='\033[0;4;47;31m'
-
-export GREEN_ON_GRAY='\033[0;47;32m'
-export GREENUL_ON_GRAY='\033[0;4;47;32m'
-
-export YELLOW_ON_GRAY='\033[0;47;33m'
-export YELLOWUL_ON_GRAY='\033[0;4;47;33m'
-
-export BLUE_ON_GRAY='\033[0;47;34m'
-export BLUEUL_ON_GRAY='\033[0;4;47;34m'
-
-export MAGENTA_ON_GRAY='\033[0;47;35m'
-export MAGENTAUL_ON_GRAY='\033[0;4;47;35m'
-
-export CYAN_ON_GRAY='\033[0;47;36m'
-export CYANUL_ON_GRAY='\033[0;4;47;36m'
-
-export WHITE_ON_GRAY='\033[0;47;1;37m'
-
-# colors below are used in PS1 for color prompt
-
-export PS_NOCOLOR='\[\033[00m\]'
-
-export PS_BLACK='\[\033[0;1m\]'
-
-export PS_RED='\[\033[0;31m\]'
-export PS_REDUL='\[\033[0;4;31m\]'
-
-export PS_GREEN='\[\033[0;32m\]'
-export PS_GREENUL='\[\033[0;4;32m\]'
-
-export PS_YELLOW='\[\033[0;33m\]'
-export PS_YELLOWUL='\[\033[0;4;33m\]'
-
-export PS_BLUE='\[\033[0;34m\]'
-export PS_BLUEUL='\[\033[0;4;34m\]'
-
-export PS_MAGENTA='\[\033[0;35m\]'
-export PS_MAGENTAUL='\[\033[0;4;35m\]'
-
-export PS_CYAN='\[\033[0;36m\]'
-export PS_CYANUL='\[\033[0;4;36m\]'
-
-export PS_GRAY='\[\033[0;37m\]'
-export PS_GRAYUL='\[\033[0;4;37m\]'
-
-export PS_WHITE='\[\033[0;1;37m\]'
-
-# colors on gray
-
-export PS_BLACK_ON_GRAY='\[\033[0;47;1m\]'
-
-export PS_RED_ON_GRAY='\[\033[0;47;31m\]'
-export PS_REDUL_ON_GRAY='\[\033[0;4;47;31m\]'
-
-export PS_GREEN_ON_GRAY='\[\033[0;47;32m\]'
-export PS_GREENUL_ON_GRAY='\[\033[0;4;47;32m\]'
-
-export PS_YELLOW_ON_GRAY='\[\033[0;47;38;5;227m\]'
-export PS_YELLOWUL_ON_GRAY='\[\033[0;47;38;5;227m\]'
-export PS_YELLOW_ON_GREY='\[\033[0m\033[48;5;250m\033[38;5;227m\]'
 export PS_YELLOWUL_ON_GREY='\[\033[0m\033[48;5;250m\033[4;38;5;227m\]'
-
-export PS_BLUE_ON_GRAY='\[\033[0;47;34m\]'
-export PS_BLUEUL_ON_GRAY='\[\033[0;4;47;34m\]'
-
 export PS_MAGENTA_ON_GRAY='\[\033[0;47;35m\]'
-export PS_MAGENTAUL_ON_GRAY='\[\033[0;4;47;35m\]'
-
-export PS_CYAN_ON_GRAY='\[\033[0;47;36m\]'
-export PS_CYANUL_ON_GRAY='\[\033[0;4;47;36m\]'
-
-export PS_WHITE_ON_GRAY='\[\033[0;47;1;37m\]'
-
-export PS_TIME=$PS_REDUL_ON_GRAY'[\t]'$PS_NOCOLOR
-#export PS_USER=$PS_YELLOWUL_ON_GREY'\u'$PS_GREENUL_ON_GREY'@'$PS_NOCOLOR
+export PS_BLUE_ON_GRAY='\[\033[0;47;34m\]'
+export PS_NOCOLOR='\[\033[00m\]'
+export PS_RED_ON_GRAY='\[\033[0;47;31m\]'
+export PS_GREENUL_ON_GRAY='\[\033[0;4;47;32m\]'
 export PS_USER=$PS_YELLOWUL_ON_GREY'\u'$PS_NOCOLOR
-export PS_HOST=$PS_YELLOWUL_ON_GREY'\h'$PS_NOCOLOR
-export PS_CWD=$PS_BLUE_ON_GRAY'\w'$PS_NOCOLOR
 export PS_SEP=$PS_MAGENTA_ON_GRAY':'$PS_NOCOLOR
-export PS1="$PS_USER$PS_SEP$PS_CWD$PS_GREENUL_ON_GRAY\$(parse_git_branch)$PS_RED_ON_GRAY\$$PS_NOCOLOR "
+export PS_CWD=$PS_BLUE_ON_GRAY'\w'$PS_NOCOLOR
+export PS1="$PS_USER$PS_SEP$PS_CWD$PS_GREENUL_ON_GRAY$PS_RED_ON_GRAY\$$PS_NOCOLOR "
+
 export PATH=/usr/local/bin:$PATH
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias lh='ls -alhF'
+alias l='ls -CF'
 EOF
 
     . ~/.bashrc
 }
+
+_blfs_set_user_defaults() {
+    # copied from Ubuntu 18.04
+
+    cat > /etc/skel/.profile << "EOF"
+if [ -n "$BASH_VERSION" ]; then
+    if [ -f "$HOME/.bashrc" ]; then
+        . "$HOME/.bashrc"
+    fi
+fi
+EOF
+
+    cat > /etc/skel/.bash_logout << "EOF"
+# ~/.bash_logout: executed by bash(1) when login shell exits.
+# when leaving the console clear the screen to increase privacy
+if [ "$SHLVL" = 1 ]; then
+    [ -x /usr/bin/clear_console ] && /usr/bin/clear_console -q
+fi
+EOF
+
+    cat > /etc/skel/.bashrc << "EOF"
+# don't put duplicate lines or lines starting with space in the history. See bash(1) for more options
+HISTCONTROL=ignoreboth
+
+# append to the history file, don't overwrite it
+shopt -s histappend
+
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
+
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+alias ls='ls --color=auto'
+alias grep='grep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias egrep='egrep --color=auto'
+alias ll='ls -alF'
+alias lh='ls -alhF'
+alias la='ls -A'
+alias l='ls -CF'
+EOF
+
+}
+
+_blfs_add_user_yyu() {
+    useradd -m yyu
+}
+
