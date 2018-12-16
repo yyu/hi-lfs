ea4983514458b2e2f9ac14ff16a4a1ef9835c103
y² <yy@yuyuan.org>
2018-12-16 03:08:16 -0800

    libdrm, funcsigs, Beaker, MarkupSafe, Mako, libva, libvdpau, libxml2, Wayland{Protocols}, Mesa, xbitmaps, Xorg Apps, xcursor themes, Xorg Fonts, XKeyboardConfig, Pixman, libepoxy, Xorg Server

--- lfs/8.3-systemd/blfs/020-nonroot.sh
+++ lfs/8.3-systemd/blfs/020-nonroot.sh
@@ -416,6 +416,398 @@ _x_install_xcb_util_cursor_() {
     popd; _blfs_cleanup $url; set +x; set +v; set +e
 }
 
+_x_install_libdrm_() {
+    set -e; set -v; set -x; url=https://dri.freedesktop.org/libdrm/libdrm-2.4.93.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    mkdir build &&
+    cd    build &&
+
+    meson --prefix=$XORG_PREFIX -Dudev=true &&
+    ninja
+    sudo ninja install
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_funcsigs_() {
+    set -e; set -v; set -x; url=https://files.pythonhosted.org/packages/source/f/funcsigs/funcsigs-1.0.2.tar.gz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    sudo python setup.py install --optimize=1
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_beaker_() {
+    set -e; set -v; set -x; url=https://files.pythonhosted.org/packages/source/B/Beaker/Beaker-1.10.0.tar.gz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    sudo python setup.py install --optimize=1
+    sudo python3 setup.py install --optimize=1
+
+    popd; set +x; set +v; set +e
+}
+
+_x_install_MarkupSafe_() {
+    set -e; set -v; set -x; url=https://files.pythonhosted.org/packages/source/M/MarkupSafe/MarkupSafe-1.0.tar.gz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    sudo python setup.py build
+    sudo python setup.py install --optimize=1
+    sudo python3 setup.py build
+    sudo python3 setup.py install --optimize=1
+
+    popd; set +x; set +v; set +e
+}
+
+_x_install_mako_() {
+    set -e; set -v; set -x; url=https://files.pythonhosted.org/packages/source/M/Mako/Mako-1.0.4.tar.gz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    sudo python setup.py install --optimize=1
+    sudo sed -i "s:mako-render:&3:g" setup.py
+    sudo python3 setup.py install --optimize=1
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_libva_() {
+    set -e; set -v; set -x;
+
+    url=https://github.com/intel/libva/releases/download/2.2.0/libva-2.2.0.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+    ./configure $XORG_CONFIG &&
+    make
+    sudo make install
+    popd
+
+    sleep 10
+    
+    url=https://github.com/intel/intel-vaapi-driver/releases/download/2.2.0/intel-vaapi-driver-2.2.0.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+    ./configure $XORG_CONFIG &&
+    make
+    sudo make install
+    popd
+
+    set +x; set +v; set +e
+}
+
+_x_install_libvdpau_() {
+    set -e; set -v; set -x; url=https://people.freedesktop.org/~aplattner/vdpau/libvdpau-1.1.1.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure $XORG_CONFIG \
+                --docdir=/usr/share/doc/libvdpau-1.1.1 &&
+    make
+    make check
+    sudo make install
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_libxml2_() {
+    set -e; set -v; set -x; url=http://xmlsoft.org/sources/libxml2-2.9.8.tar.gz
+    pushd $WD
+    wget http://www.w3.org/XML/Test/xmlts20130923.tar.gz
+    wget http://www.linuxfromscratch.org/patches/blfs/8.3/libxml2-2.9.8-python3_hack-1.patch
+    _blfs_download_extract_and_enter $url
+
+    patch -Np1 -i ../libxml2-2.9.8-python3_hack-1.patch
+    sed -i '/_PyVerify_fd/,+1d' python/types.c
+    ./configure --prefix=/usr    \
+                --disable-static \
+                --with-history   \
+                --with-python=/usr/bin/python3 &&
+    make
+    #tar xf ../xmlts20130923.tar.gz
+
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Wayland_() {
+    set -e; set -v; set -x; url=https://wayland.freedesktop.org/releases/wayland-1.15.0.tar.xz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+    ./configure --prefix=/usr    \
+                --disable-static \
+                --disable-documentation &&
+    make
+    make check
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Wayland_Protocols_() {
+    set -e; set -v; set -x; url=https://wayland.freedesktop.org/releases/wayland-protocols-1.15.tar.xz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure --prefix=/usr &&
+    make
+    make check
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Mesa_() {
+    set -e; set -v; set -x; url=https://mesa.freedesktop.org/archive/mesa-18.1.6.tar.xz
+    pushd $WD
+    wget http://www.linuxfromscratch.org/patches/blfs/8.3/mesa-18.1.6-add_xdemos-1.patch
+    _blfs_download_extract_and_enter $url
+
+    patch -Np1 -i ../mesa-18.1.6-add_xdemos-1.patch
+    GLL_DRV="i915,r600,nouveau,radeonsi,svga,swrast"
+    ./configure CFLAGS='-O2' CXXFLAGS='-O2' LDFLAGS=-lLLVM \
+                --prefix=$XORG_PREFIX              \
+                --sysconfdir=/etc                  \
+                --enable-texture-float             \
+                --enable-osmesa                    \
+                --enable-xa                        \
+                --enable-glx-tls                   \
+                --with-platforms="drm,x11,wayland" \
+                --with-gallium-drivers=$GLL_DRV    &&
+
+    unset GLL_DRV &&
+
+    make
+    make -C xdemos DEMOS_PREFIX=$XORG_PREFIX
+    make -k check
+    sudo make install
+
+    install -v -dm755 /usr/share/doc/mesa-18.1.6 &&
+    cp -rfv docs/* /usr/share/doc/mesa-18.1.6
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_xbitmaps_() {
+    set -e; set -v; set -x; url=https://www.x.org/pub/individual/data/xbitmaps-1.1.2.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure $XORG_CONFIG
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Xorg_applications_() {
+    set -e; set -v; set -x;
+    pushd $WD
+
+    cat > app-7.md5 << "EOF"
+3b9b79fa0f9928161f4bad94273de7ae  iceauth-1.0.8.tar.bz2
+c4a3664e08e5a47c120ff9263ee2f20c  luit-1.1.1.tar.bz2
+18c429148c96c2079edda922a2b67632  mkfontdir-1.0.7.tar.bz2
+987c438e79f5ddb84a9c5726a1610819  mkfontscale-1.1.3.tar.bz2
+e475167a892b589da23edf8edf8c942d  sessreg-1.1.1.tar.bz2
+2c47a1b8e268df73963c4eb2316b1a89  setxkbmap-1.3.1.tar.bz2
+3a93d9f0859de5d8b65a68a125d48f6a  smproxy-1.0.6.tar.bz2
+f0b24e4d8beb622a419e8431e1c03cd7  x11perf-1.6.0.tar.bz2
+f3f76cb10f69b571c43893ea6a634aa4  xauth-1.0.10.tar.bz2
+d50cf135af04436b9456a5ab7dcf7971  xbacklight-1.2.2.tar.bz2
+9956d751ea3ae4538c3ebd07f70736a0  xcmsdb-1.0.5.tar.bz2
+b58a87e6cd7145c70346adad551dba48  xcursorgen-1.0.6.tar.bz2
+8809037bd48599af55dad81c508b6b39  xdpyinfo-1.3.2.tar.bz2
+480e63cd365f03eb2515a6527d5f4ca6  xdriinfo-1.0.6.tar.bz2
+249bdde90f01c0d861af52dc8fec379e  xev-1.2.2.tar.bz2
+90b4305157c2b966d5180e2ee61262be  xgamma-1.0.6.tar.bz2
+f5d490738b148cb7f2fe760f40f92516  xhost-1.0.7.tar.bz2
+6a889412eff2e3c1c6bb19146f6fe84c  xinput-1.6.2.tar.bz2
+12610df19df2af3797f2c130ee2bce97  xkbcomp-1.4.2.tar.bz2
+c747faf1f78f5a5962419f8bdd066501  xkbevd-1.1.4.tar.bz2
+502b14843f610af977dffc6cbf2102d5  xkbutils-1.0.4.tar.bz2
+938177e4472c346cf031c1aefd8934fc  xkill-1.0.5.tar.bz2
+5dcb6e6c4b28c8d7aeb45257f5a72a7d  xlsatoms-1.1.2.tar.bz2
+4fa92377e0ddc137cd226a7a87b6b29a  xlsclients-1.1.4.tar.bz2
+e50ffae17eeb3943079620cb78f5ce0b  xmessage-1.0.5.tar.bz2
+723f02d3a5f98450554556205f0a9497  xmodmap-1.0.9.tar.bz2
+eaac255076ea351fd08d76025788d9f9  xpr-1.0.5.tar.bz2
+4becb3ddc4674d741487189e4ce3d0b6  xprop-1.2.3.tar.bz2
+ebffac98021b8f1dc71da0c1918e9b57  xrandr-1.5.0.tar.bz2
+96f9423eab4d0641c70848d665737d2e  xrdb-1.1.1.tar.bz2
+c56fa4adbeed1ee5173f464a4c4a61a6  xrefresh-1.0.6.tar.bz2
+70ea7bc7bacf1a124b1692605883f620  xset-1.2.4.tar.bz2
+5fe769c8777a6e873ed1305e4ce2c353  xsetroot-1.1.2.tar.bz2
+558360176b718dee3c39bc0648c0d10c  xvinfo-1.1.3.tar.bz2
+11794a8eba6d295a192a8975287fd947  xwd-1.0.7.tar.bz2
+9a505b91ae7160bbdec360968d060c83  xwininfo-1.1.4.tar.bz2
+79972093bb0766fcd0223b2bd6d11932  xwud-1.0.5.tar.bz2
+EOF
+
+    mkdir app &&
+    cd app &&
+    grep -v '^#' ../app-7.md5 | awk '{print $2}' | wget -i- -c \
+        -B https://www.x.org/pub/individual/app/ &&
+    md5sum -c ../app-7.md5
+    for package in $(grep -v '^#' ../app-7.md5 | awk '{print $2}')
+    do
+      packagedir=${package%.tar.bz2}
+      tar -xf $package
+      pushd $packagedir
+         case $packagedir in
+           luit-[0-9]* )
+             sed -i -e "/D_XOPEN/s/5/6/" configure
+           ;;
+         esac
+
+         ./configure $XORG_CONFIG
+         make
+         sudo make install
+      popd
+      rm -rf $packagedir
+    done
+    sudo rm -f $XORG_PREFIX/bin/xkeystone
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_xcursor_themes_() {
+    set -e; set -v; set -x; url=https://www.x.org/pub/individual/data/xcursor-themes-1.0.5.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure $XORG_CONFIG
+    make
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Xorg_Fonts_() {
+    set -e; set -v; set -x;
+    pushd $WD
+
+    cat > font-7.md5 << "EOF"
+23756dab809f9ec5011bb27fb2c3c7d6  font-util-1.3.1.tar.bz2
+0f2d6546d514c5cc4ecf78a60657a5c1  encodings-1.0.4.tar.bz2
+6d25f64796fef34b53b439c2e9efa562  font-alias-1.0.3.tar.bz2
+fcf24554c348df3c689b91596d7f9971  font-adobe-utopia-type1-1.0.4.tar.bz2
+e8ca58ea0d3726b94fe9f2c17344be60  font-bh-ttf-1.0.3.tar.bz2
+53ed9a42388b7ebb689bdfc374f96a22  font-bh-type1-1.0.3.tar.bz2
+bfb2593d2102585f45daa960f43cb3c4  font-ibm-type1-1.0.3.tar.bz2
+6306c808f7d7e7d660dfb3859f9091d2  font-misc-ethiopic-1.0.3.tar.bz2
+3eeb3fb44690b477d510bbd8f86cf5aa  font-xfree86-type1-1.0.4.tar.bz2
+EOF
+
+    mkdir font &&
+    cd font &&
+    grep -v '^#' ../font-7.md5 | awk '{print $2}' | wget -i- -c \
+        -B https://www.x.org/pub/individual/font/ &&
+    md5sum -c ../font-7.md5
+
+    for package in $(grep -v '^#' ../font-7.md5 | awk '{print $2}')
+    do
+      packagedir=${package%.tar.bz2}
+      tar -xf $package
+      pushd $packagedir
+        ./configure $XORG_CONFIG
+        make
+        sudo make install
+      popd
+      sudo rm -rf $packagedir
+    done
+
+    sudo install -v -d -m755 /usr/share/fonts                               &&
+    sudo ln -svfn $XORG_PREFIX/share/fonts/X11/OTF /usr/share/fonts/X11-OTF &&
+    sudo ln -svfn $XORG_PREFIX/share/fonts/X11/TTF /usr/share/fonts/X11-TTF
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_XKeyboardConfig_() {
+    set -e; set -v; set -x; url=https://www.x.org/pub/individual/data/xkeyboard-config/xkeyboard-config-2.24.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure $XORG_CONFIG --with-xkb-rules-symlink=xorg &&
+    make
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Pixman_() {
+    set -e; set -v; set -x; url=https://www.cairographics.org/releases/pixman-0.34.0.tar.gz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure --prefix=/usr --disable-static &&
+    make
+    make check
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_libepoxy_() {
+    set -e; set -v; set -x; url=https://github.com/anholt/libepoxy/releases/download/1.5.2/libepoxy-1.5.2.tar.xz
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure --prefix=/usr &&
+    make
+    make check
+    sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
+_x_install_Xorg_Server_() {
+    set -e; set -v; set -x; url=https://www.x.org/pub/individual/xserver/xorg-server-1.20.1.tar.bz2
+    pushd $WD
+    _blfs_download_extract_and_enter $url
+
+    ./configure $XORG_CONFIG          \
+                --enable-glamor       \
+                --enable-suid-wrapper \
+                --with-xkb-output=/var/lib/xkb &&
+    make
+    #sudo ldconfig && make check
+    sudo make install &&
+    sudo mkdir -pv /etc/X11/xorg.conf.d
+
+    echo -e "\033[1;32m**************************************************\033[0m"
+
+    popd; _blfs_cleanup $url; set +x; set +v; set +e
+}
+
 _x_install___() {
     set -e; set -v; set -x; url=
     pushd $WD
@@ -423,7 +815,10 @@ _x_install___() {
 
     ./configure $XORG_CONFIG
     make
+    #make check
     sudo make install
+
+    echo -e "\033[1;32m**************************************************\033[0m"
 
     popd; _blfs_cleanup $url; set +x; set +v; set +e
 }
@@ -511,5 +906,81 @@ _x_install_xcb_util_wm() {
 
 _x_install_xcb_util_cursor() {
     _x_wrap_  _x_install_xcb_util_cursor_ && $WRAPPER
+}
+
+_x_install_libdrm() {
+    _x_wrap_  _x_install_libdrm_ && $WRAPPER
+}
+
+_x_install_funcsigs() {
+    _x_wrap_  _x_install_funcsigs_ && $WRAPPER
+}
+
+_x_install_beaker() {
+    _x_wrap_  _x_install_beaker_ && $WRAPPER
+}
+
+_x_install_MarkupSafe() {
+    _x_wrap_  _x_install_MarkupSafe_ && $WRAPPER
+}
+
+_x_install_mako() {
+    _x_wrap_  _x_install_mako_ && $WRAPPER
+}
+
+_x_install_libva() {
+    _x_wrap_  _x_install_libva_ && $WRAPPER
+}
+
+_x_install_libvdpau() {
+    _x_wrap_  _x_install_libvdpau_ && $WRAPPER
+}
+
+_x_install_libxml2() {
+    _x_wrap_  _x_install_libxml2_ && $WRAPPER
+}
+
+_x_install_Wayland() {
+    _x_wrap_  _x_install_Wayland_ && $WRAPPER
+}
+
+_x_install_Wayland_Protocols() {
+    _x_wrap_  _x_install_Wayland_Protocols_ && $WRAPPER
+}
+
+_x_install_Mesa() {
+    _x_wrap_  _x_install_Mesa_ && $WRAPPER
+}
+
+_x_install_xbitmaps() {
+    _x_wrap_  _x_install_xbitmaps_ && $WRAPPER
+}
+
+_x_install_Xorg_applications() {
+    _x_wrap_  _x_install_Xorg_applications_ && $WRAPPER
+}
+
+_x_install_xcursor_themes() {
+    _x_wrap_  _x_install_xcursor_themes_ && $WRAPPER
+}
+
+_x_install_Xorg_Fonts() {
+    _x_wrap_  _x_install_Xorg_Fonts_ && $WRAPPER
+}
+
+_x_install_XKeyboardConfig() {
+    _x_wrap_  _x_install_XKeyboardConfig_ && $WRAPPER
+}
+
+_x_install_Pixman() {
+    _x_wrap_  _x_install_Pixman_ && $WRAPPER
+}
+
+_x_install_libepoxy() {
+    _x_wrap_  _x_install_libepoxy_ && $WRAPPER
+}
+
+_x_install_Xorg_Server() {
+    _x_wrap_  _x_install_Xorg_Server_ && $WRAPPER
 }
 
